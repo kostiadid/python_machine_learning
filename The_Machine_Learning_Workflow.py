f63cf1c7dc27bb7fd5630a3a2e0cc137f7e12881
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-12 01:52:40 +0000

    Data Preparation

--- The_Machine_Learning_Workflow.py
+++ The_Machine_Learning_Workflow.py
@@ -1,6 +1,22 @@
 import pandas as pd
-from  sklearn.datasets import load_breast_cancer
-
-cancer_data = load_breast_cancer(as_frame = True)
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+cancer_data = load_breast_cancer(as_frame=True)
 cancer_df = cancer_data.data
 cancer_df['target'] = cancer_data.target
+
+x = cancer_df.drop('target', axis=1)
+
+y = cancer_df['target']
+
+
+
+
+
+
+
+# The output of train_test_split() is a list containing 4 elements:
+# The training set features.
+# The test set features.
+# The training set labels.
+# The test set labels.
