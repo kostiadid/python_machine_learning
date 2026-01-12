4b5f86b5e74ceb036422880f42039f20b743607a
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-12 01:59:13 +0000

    train_test_split()

--- The_Machine_Learning_Workflow.py
+++ The_Machine_Learning_Workflow.py
@@ -5,12 +5,11 @@ cancer_data = load_breast_cancer(as_frame=True)
 cancer_df = cancer_data.data
 cancer_df['target'] = cancer_data.target
 
-x = cancer_df.drop('target', axis=1)
+X = cancer_df.drop('target', axis=1)
 
 y = cancer_df['target']
 
-
-
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.15,random_state=417)
 
 
 
