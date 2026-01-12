a4e5a1d9c7960a02e111155f14f09057edb2f50c
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-12 01:36:56 +0000

    Data Preparation

---
+++ The_Machine_Learning_Workflow.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from  sklearn.datasets import load_breast_cancer
+
+cancer_data = load_breast_cancer(as_frame = True)
+cancer_df = cancer_data.data
+cancer_df['target'] = cancer_data.target
