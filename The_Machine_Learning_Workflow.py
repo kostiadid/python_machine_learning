9de820cf38f649a7de953c37b1eda936cd9f4136
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-12 21:57:05 -0500

    Update The_Machine_Learning_Workflow.py

--- The_Machine_Learning_Workflow.py
+++ The_Machine_Learning_Workflow.py
@@ -1,21 +1,22 @@
 import pandas as pd
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
 cancer_data = load_breast_cancer(as_frame=True)
 cancer_df = cancer_data.data
 cancer_df['target'] = cancer_data.target
 
-X = cancer_df.drop('target', axis=1)
+X = cancer_df.drop(["target"], axis=1)
+y = cancer_df["target"]
 
-y = cancer_df['target']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=417)
 
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.15,random_state=417)
+# class LinearSVC:
+#     def __init__(self,penalty,loss, random_state):
+#             self.penalty = penalty
+#             self.loss = loss
+#             self.random_state = random_state
+        
 
-
-
-
-# The output of train_test_split() is a list containing 4 elements:
-# The training set features.
-# The test set features.
-# The training set labels.
-# The test set labels.
+model = LinearSVC(penalty="l2",loss="hinge",C=10,random_state=417)
+model.fit(X_train,y_train)
