e3c0840d223b836c7b9d8c97958e066cd08516de
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-13 20:50:24 -0500

    Replace  method instead of   loop

--- K-Nearest Neighbors_ALGORITM.py
+++ K-Nearest Neighbors_ALGORITM.py
@@ -4,15 +4,15 @@ banking_df = pd.read_csv("subscription_prediction.csv")
 # print(banking_df["y"])
 
 
-for i in  banking_df["y"]:
-    if i == "yes":
-        i = 1
-    else:
-        i = 0
+banking_df["y"] = banking_df["y"].replace({"yes": 1, "no": 0})
+
+        
+        
+        
 train_df = banking_df.sample(frac=0.85,random_state=417)
 test_df =  banking_df.drop(train_df.index)  
-print(train_df.value_counts(normalize=True))
-print(test_df.value_counts(normalize=True))
+print(train_df["y"].value_counts(normalize=True))
+print(test_df["y"].value_counts(normalize=True))
 
 
 X_train = train_df.drop(columns=["y"])
@@ -20,12 +20,3 @@ y_train = train_df["y"]
 
 X_test = test_df.drop(columns=["y"])
 y_test = test_df["y"]
-
-
-
-# The submitted code has a few issues that need to be addressed.
-
-# The target variable y in the banking_df DataFrame is not being updated with the new values (1 for "yes" and 0 for "no"). The for loop that attempts to do this is not modifying the DataFrame because it's only iterating over the values, not updating the DataFrame.
-# The value_counts(normalize=True) function is being called on the entire DataFrame (train_df and test_df) instead of the specific column y. This will calculate the distribution of all columns, not just the target variable.
-# The rest of the code seems to be correct, but it's relying on the previous steps being correct.
-# To fix these issues, you should focus on updating the y column in banking_df and then calculate the value counts for the y column specifically.
