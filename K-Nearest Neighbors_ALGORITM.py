28ab902c5ba6a75642b8f71ce3fed12035157c99
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-13 20:30:02 -0500

    Update K-Nearest Neighbors_ALGORITM.py
    Introduction to K-Nearest Neighbors   step 1

--- K-Nearest Neighbors_ALGORITM.py
+++ K-Nearest Neighbors_ALGORITM.py
@@ -1,1 +1,14 @@
+import pandas as pd
+banking_df = pd.read_csv("subscription_prediction.csv")
 
+# print(banking_df["y"])
+
+
+for i in  banking_df["y"]:
+    if i == "yes":
+        i = 1
+    else:
+        i = 0
+train_df = banking_df.sample(frac=0.85,random_state=417)
+test_df =  banking_df.drop(train_df.index)  
+print(banking_df.value_counts())
