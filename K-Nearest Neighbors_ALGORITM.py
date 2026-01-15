a8455b94803d4ef82d5ab965f3e0498c27cbac41
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-14 22:19:35 -0500

    Need to get familiar with  datasets and how to work with them in pandas
    Need to get familiar with  datasets and how to work with them in pandas
    
    
    
    
    will come back  .

--- K-Nearest Neighbors_ALGORITM.py
+++ K-Nearest Neighbors_ALGORITM.py
@@ -4,10 +4,7 @@ banking_df = pd.read_csv("subscription_prediction.csv")
 # print(banking_df["y"])
 
 
-banking_df["y"] = banking_df["y"].replace({"yes": 1, "no": 0})
-
-        
-        
+banking_df["y"] = banking_df["y"].replace({"yes": 1, "no": 0})        
         
 train_df = banking_df.sample(frac=0.85,random_state=417)
 test_df =  banking_df.drop(train_df.index)  
